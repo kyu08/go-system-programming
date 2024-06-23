089d750b9dafd37c8b0f2af6447bdc80a8246adb
kyu08 <49891479+kyu08@users.noreply.github.com>
2024-06-23 13:53:13 +0900

    6-tcp-socket-http

---
+++ 6-tcp-socket-http/server/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+)
+
+// TCPソケットを使ったHTTPサーバ
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8888")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Server is running at localhost:8888\n\n")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		go func() {
+			fmt.Printf("Accept %v\n", conn.RemoteAddr())
+			// リクエストを読み込む
+			request, err := http.ReadRequest(bufio.NewReader(conn))
+			if err != nil {
+				panic(err)
+			}
+			// リクエストをデバッグ表示
+			dump, err := httputil.DumpRequest(request, true)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(dump))
+
+			// レスポンスを返す
+			response := http.Response{
+				StatusCode: 200,
+				ProtoMajor: 1,
+				ProtoMinor: 0,
+				Body:       io.NopCloser(strings.NewReader("Hello, World\n")),
+			}
+			response.Write(conn)
+			conn.Close()
+		}()
+	}
+}
