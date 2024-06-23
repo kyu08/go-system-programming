11cff665db16f7cdcd2062a015b6da205ab028af
kyu08 <49891479+kyu08@users.noreply.github.com>
2024-06-23 20:40:25 +0900

    6-tcp-socket-http gzip

--- 6-tcp-socket-http/server/main.go
+++ 6-tcp-socket-http/server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"compress/gzip"
 	"fmt"
 	"io"
 	"net"
@@ -10,6 +12,64 @@ import (
 	"strings"
 	"time"
 )
+
+func isGZiptAcceptable(request *http.Request) bool {
+	return strings.Contains(strings.Join(request.Header["Accept-Encoding"], ","), "gzip")
+}
+
+func processSession(conn net.Conn) {
+	fmt.Printf("Accept %v\n", conn.RemoteAddr())
+	defer conn.Close()
+	for {
+		// タイムアウトを設定
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+
+		// リクエストを読み込む
+		request, err := http.ReadRequest(bufio.NewReader(conn))
+		if err != nil {
+			// タイムアウトもしくはソケットクローズ時は終了
+			// それ以外はエラーにする
+			neterr, ok := err.(net.Error)
+			if ok && neterr.Timeout() {
+				fmt.Println("Timeout")
+				break
+			} else if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+		// リクエストをデバッグ表示
+		dump, err := httputil.DumpRequest(request, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+
+		response := http.Response{
+			StatusCode: 200,
+			ProtoMajor: 1,
+			ProtoMinor: 1,
+			Header:     make(http.Header),
+		}
+		if isGZiptAcceptable(request) {
+			content := "Hello World (gzipped)\n"
+			// コンテンツをgzip化して転送
+			var buffer bytes.Buffer
+			writer := gzip.NewWriter(&buffer)
+			io.WriteString(writer, content)
+			writer.Close()
+			response.Body = io.NopCloser(&buffer)
+			response.ContentLength = int64(buffer.Len())
+			response.Header.Set("Content-Encoding", "gzip")
+		} else {
+			content := "Hello World\n"
+			response.Body = io.NopCloser(strings.NewReader(content))
+			response.ContentLength = int64(len(content))
+		}
+
+		response.Write(conn)
+	}
+}
 
 // TCPソケットを使ったHTTPサーバ
 func main() {
@@ -25,50 +85,6 @@ func main() {
 			panic(err)
 		}
 
-		go func() {
-			defer conn.Close()
-			fmt.Printf("Accept %v\n", conn.RemoteAddr())
-			for {
-				// タイムアウトを設定
-				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-
-				// リクエストを読み込む
-				request, err := http.ReadRequest(bufio.NewReader(conn))
-				if err != nil {
-					// タイムアウトもしくはソケットクローズ時は終了
-					// それ以外はエラーにする
-					neterr, ok := err.(net.Error)
-					if ok && neterr.Timeout() {
-						fmt.Println("Timeout")
-						break
-					} else if err == io.EOF {
-						break
-					}
-					panic(err)
-				}
-				// リクエストをデバッグ表示
-				dump, err := httputil.DumpRequest(request, true)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println(string(dump))
-
-				content := "Hello World\n"
-
-				// レスポンスを返す
-				response := http.Response{
-					StatusCode: 200,
-					ProtoMajor: 1,
-					ProtoMinor: 1,
-					// GoのResponse.Write()はHTTP/1.1より古いバージョンが使われる場合、もしくは
-					// 長さがわからない場合は`Connection: close`ヘッダーを付与してしまう。
-					// これは、クライアントが複数のレスポンスを扱うことから明確にそれぞれのレスポンスを区切る必要があるため。
-					// そのためKeep-Aliveを利用するためにはContentLengthを設定する必要がある。
-					ContentLength: int64(len(content)),
-					Body:          io.NopCloser(strings.NewReader(content)),
-				}
-				response.Write(conn)
-			}
-		}()
+		go processSession(conn)
 	}
 }
